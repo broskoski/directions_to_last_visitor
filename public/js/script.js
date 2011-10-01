bf2e6e7411470ccac9027aceba0980b9a5479a13
Charles Broskoski <broskoski@gmail.com>
2011-10-01 16:34:16 -0400

    geolocator

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
+	get_location()
 });
+
+function get_location() {
+  	navigator.geolocation.getCurrentPosition(handle_success, handle_error, {maximumAge: 75000});
+}
+
+function handle_success(position){
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	alert('hello');
+	alert('lat=' + latitude + ' long: '+ longitude);
+}
+
+function handle_error(err) {
+  
+}
