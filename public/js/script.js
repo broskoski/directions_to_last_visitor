a1d8416e5d9e1359bcf9e65544f2f4e8a5225bcb
Charles Broskoski <broskoski@gmail.com>
2011-10-07 18:20:35 -0400

    international directions

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
 	get_location();
 });
 
+function get_location() {
+	if (geo_position_js.init()) {
+	  geo_position_js.getCurrentPosition(geo_success, geo_error);
+	}
+}
+
+function geo_success(position){
+	geolat = position.coords.latitude;
+	geolng = position.coords.longitude;
+	
+	$("#lnglat").html(geolat + ", " + geolng);
+	
+	$.post("update/"+current_id, {lat: geolat, lng: geolng} );
+	
+	get_directions(geolat, geolng);
+}
+
+function geo_error(err) {
+	$('#content').show();
+	$('#content').html('<h1>Your device must provide your location<br>for this piece to function.</h1>');
+}
 function get_directions(lat, lng){
 	plng = $('#prev_lng').html();
 	plat = $('#prev_lat').html();
@@ -39,31 +60,110 @@ function get_directions(lat, lng){
 			google.maps.event.trigger(map, "resize");
 			directionsDisplay.setDirections(response);
 		}else{
-			$('#content').show();
-			$('#content').html('<h1>Directions could not be found between '+lat+','+lng+' and '+plat+', '+plng+'</h1>');
-
+			try_international_directions(origin, destination);
 		}
 	});
 }
 
-function get_location() {
-	if (geo_position_js.init()) {
-	  geo_position_js.getCurrentPosition(geo_success, geo_error);
+//disgusting, i know.
+function try_international_directions(origin, destination){
+	
+	//add map and directions elements
+	$("<div id='map2'></div>").insertAfter('#directions');
+	$("<div id='directions2'></div>").insertAfter('#map2');
+
+	var directionsServiceOrigin = new google.maps.DirectionsService();
+	var directionsDisplayOrigin = new google.maps.DirectionsRenderer();
+
+	var directionsServiceDestination = new google.maps.DirectionsService();
+	var directionsDisplayDestination = new google.maps.DirectionsRenderer();
+	
+	var myOptions = {
+		zoom:8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
+
+	var mapOrigin = new google.maps.Map(document.getElementById("map"), myOptions);
+	var mapDestination = new google.maps.Map(document.getElementById("map2"), myOptions);
+		
+	directionsDisplayOrigin.setMap(mapOrigin);
+	directionsDisplayOrigin.setPanel(document.getElementById("directions"));
+
+	directionsDisplayDestination.setMap(mapDestination);
+	directionsDisplayDestination.setPanel(document.getElementById("directions2"));
+	
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'latLng': origin}, function(resultsGeoOrigin, statusGeoOrigin) {
+		if (statusGeoOrigin == google.maps.GeocoderStatus.OK) {
+			if (resultsGeoOrigin[5]) {
+
+				originairport = resultsGeoOrigin[5].formatted_address;
+
+				var requestOrigin = {
+					origin: origin, 
+					destination: 'airport near ' + originairport,
+					travelMode: google.maps.DirectionsTravelMode.DRIVING
+				};
+
+				directionsServiceOrigin.route(requestOrigin, function(responseOrigin, statusOrigin) {
+					
+					if (statusOrigin == google.maps.DirectionsStatus.OK) {
+						geocoder.geocode({'latLng': destination}, function(resultsGeoDestination, statusGeoDestination) {
+							
+							if (statusGeoDestination == google.maps.GeocoderStatus.OK) {
+								if (resultsGeoDestination[5]) {
+
+									destinationairport = resultsGeoDestination[5].formatted_address;
+
+									var requestDestination = {
+										origin: 'airport near ' + destinationairport, 
+										destination: destination,
+										travelMode: google.maps.DirectionsTravelMode.DRIVING
+									};
+
+									directionsServiceOrigin.route(requestDestination, function(responseDestination, statusDestination) {
+										if (statusOrigin == google.maps.DirectionsStatus.OK) {
+											$('#map2').css('height', '400px').css('width', '100%').css('margin-bottom', '40px');
+											google.maps.event.trigger(mapDestination, "resize");
+											directionsDisplayDestination.setDirections(responseDestination);
+										}else{
+											show_direction_error()
+										}
+									});
+								}
+							}
+						});
+						$('#content').show();
+						$('#map').css('height', '400px').css('margin-bottom', '40px');
+						google.maps.event.trigger(mapOrigin, "resize");
+						directionsDisplayOrigin.setDirections(responseOrigin);
+					}else{
+						show_direction_error()
+					}
+				});
+				}
+		} else {
+			show_direction_error()
+		}
+	});
 }
 
-function geo_success(position){
-	geolat = position.coords.latitude;
-	geolng = position.coords.longitude;
-	
-	$("#lnglat").html(geolat + ", " + geolng);
-	
-	$.post("update/"+current_id, {lat: geolat, lng: geolng} );
-	
-	get_directions(geolat, geolng);
+function get_address(location){
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'latLng': location}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			if (results[1]) {
+				console.log(results[1].formatted_address)
+				originairport = results[1].formatted_address
+			}
+		} else {
+			show_direction_error()
+			alert("Geocoder failed due to: " + status);
+		}
+	});
 }
 
-function geo_error(err) {
+function show_direction_error(){
 	$('#content').show();
-	$('#content').html('<h1>Your device must provide your location<br>for this piece to function.</h1>');
+	$('#content').html('<h1>Directions could not be found between '+lat+','+lng+' and '+plat+', '+plng+'</h1>');
 }
