d08a9648fff05fb087aac295a0a2a8a5617b1741
Charles Broskoski <broskoski@gmail.com>
2011-10-24 13:36:36 -0400

    less general

--- public/js/script.js
+++ public/js/script.js
@@ -97,7 +97,7 @@ function try_international_directions(origin, destination){
 
 				var requestOrigin = {
 					origin: origin, 
-					destination: 'airport near ' + originairport,
+					destination: 'Airport near ' + originairport,
 					travelMode: google.maps.DirectionsTravelMode.DRIVING
 				};
 
@@ -112,7 +112,7 @@ function try_international_directions(origin, destination){
 									destinationairport = resultsGeoDestination[4].formatted_address;
 
 									var requestDestination = {
-										origin: 'airport near ' + destinationairport, 
+										origin: 'Airport near ' + destinationairport, 
 										destination: destination,
 										travelMode: google.maps.DirectionsTravelMode.DRIVING
 									};
