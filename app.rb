2d58e717153537ef42d1d085d76ab942fb532add
Charles Broskoski <broskoski@gmail.com>
2012-05-03 20:48:09 -0400

    Revert "Revert "rack session?""
    This reverts commit 3089ab01e5e258a3a7b4d0e7a259867b876d7805.

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require 'json'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/my.db")
 
+use Rack::Session::Pool, :expire_after => 2592000
+
 class Visitor 
   include DataMapper::Resource  
 
